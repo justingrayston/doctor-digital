9b935ebe79c22764dea86e6c4075d2a0d60005e1
Rohan Mahalank <rohan.m.8@gmail.com>
2018-02-06 22:14:36 +0530

    update backup
    adding patient ID while saving appointment in backup

--- edge/src/gateway/apiai-connector-backup/apiproxy/resources/jsc/action-processing.js
+++ edge/src/gateway/apiai-connector-backup/apiproxy/resources/jsc/action-processing.js
@@ -68,7 +68,7 @@ if (action === "appointment.book.confirm") {
   var clinic = body.result.contexts.find(function (element) {
     return (element.name = 'clinic' && element.parameters.clinicId);
   }).parameters;
-  path += "Appointment";
+  path += "Appointment/" + patientId;
   serviceCalloutFlag = true;
   verb = "PUT";
   var date = new Date();
